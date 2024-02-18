b9b3fd336d5dd610f7d23c4aaddab92d67164a14
Rajasekar T <sayhellotoraji01@gmail.com>
2024-02-18 18:49:01 +0530

    Login Controller works fine
    Routing of url to the html page is completed by creating LoginController class.

--- Mini_Project_1/employee_management/src/main/java/com/rajasekar_t/employee_management/controller/LoginController.java
+++ Mini_Project_1/employee_management/src/main/java/com/rajasekar_t/employee_management/controller/LoginController.java
@@ -6,8 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-	@GetMapping("/welcome")
+	@GetMapping("/log")
 	public String displayLogin() {
 		return "login";
+	}
+	
+	@GetMapping("/index")
+	public String welcome() {
+		return "index";
+	}
+	
+	@GetMapping("/add_employee")
+	public String addEmployeePage() {
+		return "add_employee";
+	}
+	
+	@GetMapping("/update_employee")
+	public String updateEmployeePage() {
+		return "update_employee";
 	}
 }
