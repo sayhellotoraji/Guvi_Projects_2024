d84d252c8629be132956f09c74b33374022f9ce6
Rajasekar T <sayhellotoraji01@gmail.com>
2024-02-19 08:57:26 +0530

    Fixed Routing issues & Still Security needs modification
    Url
    
    /login - Spring security login page
    /log - Custom login page (which is still under development)
    
    /index - Landing page which displays employees list
    /add_employee - add new employee
    /update_employee - update details

--- Mini_Project_1/employee_management/src/main/java/com/rajasekar_t/employee_management/controller/LoginController.java
+++ Mini_Project_1/employee_management/src/main/java/com/rajasekar_t/employee_management/controller/LoginController.java
@@ -1,18 +1,26 @@
 package com.rajasekar_t.employee_management.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.rajasekar_t.employee_management.service.EmployeeService;
 
 @Controller
 public class LoginController {
 
+	@Autowired
+	EmployeeService employeeService;
+	
 	@GetMapping("/log")
 	public String displayLogin() {
 		return "login";
 	}
 	
 	@GetMapping("/index")
-	public String welcome() {
+	public String welcome(Model model) {
+		model.addAttribute("listEmployees", employeeService.findAllEmployees());
 		return "index";
 	}
 	
