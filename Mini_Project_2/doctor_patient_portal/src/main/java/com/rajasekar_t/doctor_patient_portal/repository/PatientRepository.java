852e5a4eaf3d3847ad4aa701f99569dbc8de05c4
Rajasekar T <sayhellotoraji01@gmail.com>
2024-02-28 17:47:07 +0530

    Updated the tag name in PatientRepository

--- Mini_Project_2/doctor_patient_portal/src/main/java/com/rajasekar_t/doctor_patient_portal/repository/PatientRepository.java
+++ Mini_Project_2/doctor_patient_portal/src/main/java/com/rajasekar_t/doctor_patient_portal/repository/PatientRepository.java
@@ -7,7 +7,7 @@ import com.rajasekar_t.doctor_patient_portal.model.Patient;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-@Tag(name = "Patient Record - Rest API Controllers", description = "Patient Record API")
+@Tag(name = "Record: Patient - Rest API Controllers", description = "Patient Record API")
 @RepositoryRestResource(collectionResourceRel = "patient", path="patient")
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
 
