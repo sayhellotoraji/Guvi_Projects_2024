5fe34b551b19cfe9728677d78435a274bb38f86d
Rajasekar T <sayhellotoraji01@gmail.com>
2024-02-28 17:01:00 +0530

    @Tag added for Patient Repository Controller
    Specify the name & description of the controller in Swagger

--- Mini_Project_2/doctor_patient_portal/src/main/java/com/rajasekar_t/doctor_patient_portal/repository/PatientRepository.java
+++ Mini_Project_2/doctor_patient_portal/src/main/java/com/rajasekar_t/doctor_patient_portal/repository/PatientRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.rajasekar_t.doctor_patient_portal.model.Patient;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Patient Record - Rest API Controllers", description = "Patient Record API")
 @RepositoryRestResource(collectionResourceRel = "patient", path="patient")
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
 
