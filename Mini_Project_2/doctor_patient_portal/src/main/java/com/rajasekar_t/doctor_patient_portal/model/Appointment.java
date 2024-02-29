05a9442754d2210e86154c0bf978274d6b4d3c23
Rajasekar T <sayhellotoraji01@gmail.com>
2024-02-29 15:05:58 +0530

    Created Appointment Model Class

---
+++ Mini_Project_2/doctor_patient_portal/src/main/java/com/rajasekar_t/doctor_patient_portal/model/Appointment.java
@@ -0,0 +1,10 @@
+package com.rajasekar_t.doctor_patient_portal.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity(name = "appointment")
+@Table
+public class Appointment {
+
+}
