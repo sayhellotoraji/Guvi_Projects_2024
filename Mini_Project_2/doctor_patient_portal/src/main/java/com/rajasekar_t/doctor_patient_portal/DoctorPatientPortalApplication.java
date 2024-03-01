f05ac0d1d4b7670d50514ee1ca0b869941e3ed41
Rajasekar T <sayhellotoraji01@gmail.com>
2024-03-01 13:52:22 +0530

    Annotation Configuration tested for removing property in HateOAS

--- Mini_Project_2/doctor_patient_portal/src/main/java/com/rajasekar_t/doctor_patient_portal/DoctorPatientPortalApplication.java
+++ Mini_Project_2/doctor_patient_portal/src/main/java/com/rajasekar_t/doctor_patient_portal/DoctorPatientPortalApplication.java
@@ -3,6 +3,11 @@ package com.rajasekar_t.doctor_patient_portal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// To remove HateOAS embedded property in Json Response - Not working
+// Spring Data Rest Without HATEOAS - exclude RepositoryRestMvcAutoConfiguration
+// @SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
+
 @SpringBootApplication
 public class DoctorPatientPortalApplication {
 
