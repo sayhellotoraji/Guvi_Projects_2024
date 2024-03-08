d4c11e878182e58b3f06d27c411181e1720654ba
Rajasekar T <sayhellotoraji01@gmail.com>
2024-03-08 20:12:43 +0530

    Find by Passenger id - Repository

--- Capstone_Project/bus_ticket_booking/src/main/java/com/rajasekar_t/bus_ticket_booking/repository/BookingRepository.java
+++ Capstone_Project/bus_ticket_booking/src/main/java/com/rajasekar_t/bus_ticket_booking/repository/BookingRepository.java
@@ -1,6 +1,9 @@
 package com.rajasekar_t.bus_ticket_booking.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.rajasekar_t.bus_ticket_booking.model.Booking;
@@ -11,4 +14,5 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RepositoryRestResource(collectionResourceRel = "booking", path="booking")
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
 
+	List<Booking> findByPassengerId(@Param("passengerId") int passengerId);
 }
