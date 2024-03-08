e9eaafc024005d273026f31f8bfe57f1b246a391
Rajasekar T <sayhellotoraji01@gmail.com>
2024-03-08 20:10:20 +0530

    Use of RequestParam in Search Bus based on Conditions
    Condtions:
    Date,
    From & To Location

--- Capstone_Project/bus_ticket_booking/src/main/java/com/rajasekar_t/bus_ticket_booking/controller/BusController.java
+++ Capstone_Project/bus_ticket_booking/src/main/java/com/rajasekar_t/bus_ticket_booking/controller/BusController.java
@@ -1,6 +1,7 @@
 package com.rajasekar_t.bus_ticket_booking.controller;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.rajasekar_t.bus_ticket_booking.model.Bus;
 import com.rajasekar_t.bus_ticket_booking.repository.BusRepository;
@@ -25,15 +27,15 @@ public class BusController {
 	public String busSchedules(@PathVariable("passengerId") int id, Model model) {
 		List<Bus> buses = busRepo.findAll();
 		model.addAttribute("buses", buses);
-		
 		model.addAttribute("pid", id);
+		
 		return "bus_schedules";
 	}
 	
 	
-	@GetMapping({ "busschedules/{sdate}/{fromLoc}/{toLoc}" })
-	public String busSchedulesSearch(@PathVariable("sdate") LocalDate sdate, @PathVariable("fromLoc") String fromLoc, @PathVariable("toLoc") String toLoc,  Model model) {
-		List<Bus> buses = busRepo.findByFromLocAndToLocAndStartDate(fromLoc, toLoc, sdate);
+	@GetMapping({ "busschedules" })
+	public String busSchedulesSearch(@RequestParam("sDate") LocalDate sDate, @RequestParam("fromLoc") String fromLoc, @RequestParam("toLoc") String toLoc,  Model model) {
+		List<Bus> buses = busRepo.findByFromLocAndToLocAndStartDate(fromLoc, toLoc, sDate);
 		model.addAttribute("buses", buses);
 		
 		return "bus_schedules";
